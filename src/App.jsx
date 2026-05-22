95a5e918fbcf5a7f49066dfbfa00e22d8a817bb5
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-05-21 22:13:44 -0700

    Added game win/lose state

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ export default function AssemblyEndgame() {
 	userGuesses.forEach((guess) => {
 		if (!currentWord.includes(guess)) wrongGuessCount++;
 	});
-	const isGameOver = wrongGuessCount > 7;
+	const isGameWon = currentWord
+		.split("")
+		.every((letter) => userGuesses.has(letter));
+	const isGameLost = wrongGuessCount >= languages.length - 1;
+	const isGameOver = isGameWon || isGameLost;
 
 	// Alphabet for the keyboard
 	const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -67,6 +71,11 @@ export default function AssemblyEndgame() {
 		);
 	});
 
+	const gameStatusClass = clsx("game-status", {
+		won: isGameWon,
+		lost: isGameLost,
+	});
+
 	// Handle letter guess
 	function handleGuess(letter) {
 		setUserGuesses((prev) => new Set(prev).add(letter));
@@ -82,9 +91,18 @@ export default function AssemblyEndgame() {
 					from Assembly!
 				</p>
 			</header>
-			<section className="game-status">
-				<h2>You win!</h2>
-				<p>Well done! 🎉</p>
+			<section className={gameStatusClass}>
+				{isGameOver ? (
+					isGameWon ? (
+						<>
+							<h2>You win!</h2> <p>Well done!</p>
+						</>
+					) : (
+						<>
+							<h2>You lose!</h2> <p>Better start learning Assembly!</p>
+						</>
+					)
+				) : null}
 			</section>
 			<section className="language-chips">{languageElements}</section>
 			<section className="word">{wordElements}</section>
